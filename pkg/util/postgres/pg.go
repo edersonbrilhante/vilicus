3f821dfda26cba1d3db038981734500ce0663d2a
Ederson Brilhante <ederson.brilhante@grandparade.co.uk>
2021-01-20 21:05:07 +0100

    feat: add postgres

---
+++ pkg/util/postgres/pg.go
@@ -0,0 +1,50 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-pg/pg/v10"
+	// DB adapter
+	_ "github.com/lib/pq"
+)
+
+type dbLogger struct{}
+
+// BeforeQuery hooks before pg queries
+func (d dbLogger) BeforeQuery(c context.Context, q *pg.QueryEvent) (context.Context, error) {
+	return c, nil
+}
+
+// AfterQuery hooks after pg queries
+func (d dbLogger) AfterQuery(c context.Context, q *pg.QueryEvent) error {
+	query, err := q.FormattedQuery()
+	fmt.Println(string(query))
+	return err
+}
+
+// New creates new database connection to a postgres database
+func New(psn string, timeout int, enableLog bool) (*pg.DB, error) {
+	u, err := pg.ParseURL(psn)
+	if err != nil {
+		return nil, err
+	}
+
+	db := pg.Connect(u)
+
+	_, err = db.Exec("SELECT 1")
+	if err != nil {
+		return nil, err
+	}
+
+	if timeout > 0 {
+		db = db.WithTimeout(time.Second * time.Duration(timeout))
+	}
+
+	if enableLog {
+		db.AddQueryHook(dbLogger{})
+	}
+
+	return db, nil
+}
