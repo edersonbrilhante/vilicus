1c616e6b903d3c8ea38d8c44a5330805e769d268
Ederson Brilhante <ederson.brilhante@grandparade.co.uk>
2021-01-22 12:29:51 +0100

    feat: add method Update to update analysis in db

--- pkg/api/analysis/platform/pgsql/analysis.go
+++ pkg/api/analysis/platform/pgsql/analysis.go
@@ -5,6 +5,7 @@ import (
 	"github.com/go-pg/pg/v10/orm"
 )
 
+// Analysis represents the client for analysis table
 type Analysis struct{}
 
 // Create creates a new analysis on database
@@ -16,10 +17,15 @@ func (a Analysis) Create(db orm.DB, al ccvs.Analysis) (ccvs.Analysis, error) {
 
 // View returns single analysis by ID
 func (a Analysis) View(db orm.DB, id string) (ccvs.Analysis, error) {
-
 	al := ccvs.Analysis{ID: id}
-
 	err := db.Model(&al).WherePK().Select()
 
 	return al, err
 }
+
+// Update updates analysis's info
+func (a Analysis) Update(db orm.DB, al ccvs.Analysis) error {
+	return db.Update(&al)
+}
+
+
