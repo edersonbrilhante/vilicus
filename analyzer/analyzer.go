3b0291c71709f0acd8d81dff04aca26baa556e07
Ederson Brilhante <ederson.brilhante@grandparade.co.uk>
2021-01-29 20:51:13 +0100

    refactor: add module analyzer

---
+++ analyzer/analyzer.go
@@ -0,0 +1,47 @@
+package analyzer
+
+import (
+	"sync"
+
+	"github.com/edersonbrilhante/ccvs"
+	"github.com/edersonbrilhante/ccvs/pkg/util/config"
+
+	// anchoreengine "github.com/edersonbrilhante/ccvs/analyzer/anchoreengine"
+	clair "github.com/edersonbrilhante/ccvs/analyzer/clair"
+)
+
+// StartAnalysis is function to execute analysis
+func StartAnalysis(v *config.Vendors, al *ccvs.Analysis) {
+	var wg sync.WaitGroup
+
+	aa := []analyzer{
+		// &anchoreengine.Anchore{Config: v.AnchoreEngine},
+		&clair.Clair{Config: v.Clair},
+	}
+
+	for _, a := range aa {
+		wg.Add(1)
+		go start(a, al, &wg)
+	}
+
+	wg.Wait()
+}
+
+type analyzer interface {
+	Analyzer(*ccvs.Analysis) error
+	Parser() error
+}
+
+func start(v analyzer, al *ccvs.Analysis, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	err := v.Analyzer(al)
+	if err != nil {
+		al.Errors = append(al.Errors, err.Error())
+		return
+	}
+	err = v.Parser()
+	if err != nil {
+		al.Errors = append(al.Errors, err.Error())
+	}
+}
