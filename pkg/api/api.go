5502ce363cd9781782afddff7d735bc919a5d3dc
Ederson Brilhante <ederson.brilhante@grandparade.co.uk>
2021-02-10 18:53:19 +0100

    feat: add hc

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -6,6 +6,7 @@ import (
 	"github.com/edersonbrilhante/vilicus/pkg/api/analysis"
 	al "github.com/edersonbrilhante/vilicus/pkg/api/analysis/logging"
 	at "github.com/edersonbrilhante/vilicus/pkg/api/analysis/transport"
+	ht "github.com/edersonbrilhante/vilicus/pkg/api/healthcheck/transport"
 	"github.com/edersonbrilhante/vilicus/pkg/util/config"
 	"github.com/edersonbrilhante/vilicus/pkg/util/postgres"
 	"github.com/edersonbrilhante/vilicus/pkg/util/server"
@@ -27,6 +28,7 @@ func Start(cfg *config.Configuration) error {
 	v1 := e.Group("")
 
 	at.NewHTTP(al.New(analysis.Initialize(db, cfg.Vendors), log), v1)
+	ht.NewHTTP(v1)
 
 	server.Start(e, &server.Config{
 		Port:                cfg.Server.Port,
