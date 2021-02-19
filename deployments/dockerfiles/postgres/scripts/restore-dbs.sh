2937cb66385d4e79a2ea1927474ab7f6d99920aa
Ederson Brilhante <ederson.brilhante@grandparade.co.uk>
2021-02-19 23:57:35 +0100

    fix: fix restore-dbs

--- deployments/dockerfiles/postgres/scripts/restore-dbs.sh
+++ deployments/dockerfiles/postgres/scripts/restore-dbs.sh
@@ -9,14 +9,17 @@ function restore_database() {
 	tar -xf /opt/vilicus/data/$database.tar.gz --directory /opt/vilicus/data
 	
 	echo "Restoring database '$database'"
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" < "/opt/vilicus/data/$database.sql"
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d $database < "/opt/vilicus/data/$database.sql"
 }
 
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
+	mkdir -p $PGDATA
+	chown -R postgres:postgres $PGDATA
 	echo "Multiple database restore requested: $POSTGRES_MULTIPLE_DATABASES"
 	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
 		restore_database $db &
 	done;
 	wait
+	rm /opt/vilicus/data/*
 	echo "Multiple databases restored"
 fi
