35df0efa1d8e7ae2fb88c5f2de3be7dc3d2bd021
edersonbrilhante <contato@edersonbrilhante.com.br>
2021-04-09 21:24:41 +0200

    Updates

---
+++ precache-manifest.9cf5770eecb63a09c03f52513028a9a8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af6eb0345ca1ccc5272474ca7fd00367",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34e4e2bdd28a8c4a3acd",
+    "url": "/static/css/main.da37c10d.chunk.css"
+  },
+  {
+    "revision": "55494fbc3c4cf9b54760",
+    "url": "/static/js/2.593868ce.chunk.js"
+  },
+  {
+    "revision": "0def066106b5692cd5c135dcafc5767d",
+    "url": "/static/js/2.593868ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34e4e2bdd28a8c4a3acd",
+    "url": "/static/js/main.30bdd5d0.chunk.js"
+  },
+  {
+    "revision": "cc26633d4e576aeb8bf5",
+    "url": "/static/js/runtime-main.7b22b188.js"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/static/media/feature-tile-icon-02.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/static/media/feature-tile-icon-03.6a177696.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/static/media/feature-tile-icon-05.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/static/media/feature-tile-icon-06.66f37ba5.svg"
+  },
+  {
+    "revision": "da30adff383e4b78fbe54350712553b1",
+    "url": "/static/media/github-action.da30adff.png"
+  },
+  {
+    "revision": "e2925c9c602bb98787786aaf856a5805",
+    "url": "/static/media/github-sec.e2925c9c.png"
+  },
+  {
+    "revision": "203b64399699e3d78b2ff47d72325da6",
+    "url": "/static/media/gitlab-ci.203b6439.png"
+  },
+  {
+    "revision": "b01d0e5ea78691e53bad6cb194d844ce",
+    "url": "/static/media/gitlab-sec.b01d0e5e.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  }
+]);
