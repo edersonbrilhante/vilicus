04812825e103e5c75e5f5a163c7b79e157c5404d
edersonbrilhante <contato@edersonbrilhante.com.br>
2021-04-13 14:49:39 +0200

    fix: fix input in Cta secction

--- website/src/components/sections/Cta.js
+++ website/src/components/sections/Cta.js
@@ -57,7 +57,7 @@ const Cta = ({
               </h3>
           </div>
           <div className="cta-action">
-            <Input type="emailtext" label="Search Image" labelHidden hasIcon="right" placeholder="You Container Image: <domain>/<org>/<image>:<tag>">
+            <Input type="text" label="Search Image" labelHidden hasIcon="right" placeholder="You Container Image: <domain>/<org>/<image>:<tag>">
               <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
                 <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" fill="#376DF9" />
               </svg>
